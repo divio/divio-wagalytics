3b64f0ebb2fd4e8c7eb1f767262036e0eba6bccf
Daniele Procida <daniele@vurt.org>
2017-03-10 12:55:11 +0000

    Added GA_VIEW_ID to form

--- aldryn_config.py
+++ aldryn_config.py
@@ -3,10 +3,16 @@ from aldryn_client import forms
 
 
 class Form(forms.BaseForm):
+    ga_view_id = forms.CharField(
+        'Google Analytics View ID',
+        required=True,
+    )
 
     def to_settings(self, data, settings):
         settings['INSTALLED_APPS'].extend([
             'wagtailfontawesome',
         ])
+
+        settings['GA_VIEW_ID'] = data['ga_view_id']
 
         return settings
